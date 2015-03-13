f1678a74f701b1691f762c181b384120f0088f08
Redeagle48 <antoniopereira350@gmail.com>
2015-03-13 20:12:36 +0000

    Possible to go to the next messages

--- client_experiments/js/directives/QuestionDrtv.js
+++ client_experiments/js/directives/QuestionDrtv.js
@@ -3,25 +3,31 @@
  */
 angular.module('app')
     .directive('question', function QuestionDrctv() {
-    'use strict';
+        'use strict';
 
-    return {
-        restrict: 'E',
-        replace: true,
-        scope: true,
-        templateUrl: "js/directives/question.tmpl.html",
-        controllerAs: 'question',
-        controller: function ($routeParams, QuestionFactory) {
-            this.message = $routeParams;
-            
-            this.test = function () {
-                return QuestionFactory.test();
-            };
-            
-            this.messageTest = this.test();
-            
-            this.formData = {};
-            
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: true,
+            templateUrl: "js/directives/question.tmpl.html",
+            controllerAs: 'question',
+            controller: function ($routeParams, QuestionFactory, $location) {
+                this.message = $routeParams;
+
+                this.test = function () {
+                    return QuestionFactory.test();
+                };
+
+                this.messageTest = this.test();
+
+                this.formData = {};
+                
+                this.onSubmit = function onSubmit() {
+                    this.x = parseInt(this.message.id,10) + 1;
+                    console.log("At question " + this.message.id + " passing to question " + this.x);
+                    $location.path('question/' + this.x);
+                };
+
+            }
         }
-    }
-});
+    });
